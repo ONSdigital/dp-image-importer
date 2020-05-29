201ea55bc352f38e90de42a355f3dcdabb860e99
Jessica Jenkins <jess.jenkins@ons.gov.uk>
2020-05-29 16:49:25 +0100

    Refactor service with initialiser

--- service/service.go
+++ service/service.go
@@ -2,57 +2,55 @@ package service
 
 import (
 	"context"
-
-	"github.com/ONSdigital/dp-healthcheck/healthcheck"
 	"github.com/ONSdigital/dp-image-importer/api"
 	"github.com/ONSdigital/dp-image-importer/config"
-	"github.com/ONSdigital/go-ns/server"
 	"github.com/ONSdigital/log.go/log"
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
 )
 
+// Service contains all the configs, server and clients to run the Image API
 type Service struct {
-	Config      *config.Config
-	server      *server.Server
-	Router      *mux.Router
-	API         *api.API
-	HealthCheck *healthcheck.HealthCheck
+	config      *config.Config
+	server      HTTPServer
+	router      *mux.Router
+	api         *api.API
+	serviceList *ExternalServiceList
+	healthCheck HealthChecker
 }
 
 // Run the service
-func Run(buildTime, gitCommit, version string, svcErrors chan error) (*Service, error) {
-	ctx := context.Background()
+func Run(ctx context.Context, serviceList *ExternalServiceList, buildTime, gitCommit, version string, svcErrors chan error) (*Service, error) {
 	log.Event(ctx, "running service", log.INFO)
 
+	// Read config
 	cfg, err := config.Get()
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to retrieve service configuration")
 	}
 	log.Event(ctx, "got service configuration", log.Data{"config": cfg}, log.INFO)
 
+	// Get HTTP Server with collectionID checkHeader middleware
 	r := mux.NewRouter()
+	s := serviceList.GetHTTPServer(cfg.BindAddr, r)
 
-	s := server.New(cfg.BindAddr, r)
-	s.HandleOSSignals = false
-
+	// Setup the API
 	a := api.Setup(ctx, r)
 
-	versionInfo, err := healthcheck.NewVersionInfo(
-		buildTime,
-		gitCommit,
-		version,
-	)
+	// Get HealthCheck
+	hc, err := serviceList.GetHealthCheck(cfg, buildTime, gitCommit, version)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to parse version information")
+		log.Event(ctx, "could not instantiate healthcheck", log.FATAL, log.Error(err))
+		return nil, err
 	}
-	hc := healthcheck.New(versionInfo, cfg.HealthCheckCriticalTimeout, cfg.HealthCheckInterval)
-	if err := registerCheckers(ctx, &hc); err != nil {
+	if err := registerCheckers(ctx, hc); err != nil {
 		return nil, errors.Wrap(err, "unable to register checkers")
 	}
+
 	r.StrictSlash(true).Path("/health").HandlerFunc(hc.Handler)
 	hc.Start(ctx)
 
+	// Run the http server in a new go-routine
 	go func() {
 		if err := s.ListenAndServe(); err != nil {
 			svcErrors <- errors.Wrap(err, "failure in http listen and serve")
@@ -60,34 +58,64 @@ func Run(buildTime, gitCommit, version string, svcErrors chan error) (*Service, 
 	}()
 
 	return &Service{
-		Config:      cfg,
-		Router:      r,
-		API:         a,
-		HealthCheck: &hc,
+		config:      cfg,
 		server:      s,
+		router:      r,
+		api:         a,
+		serviceList: serviceList,
+		healthCheck: hc,
 	}, nil
 }
 
-// Gracefully shutdown the service
-func (svc *Service) Close(ctx context.Context) {
-	timeout := svc.Config.GracefulShutdownTimeout
+// Close gracefully shuts the service down in the required order, with timeout
+func (svc *Service) Close(ctx context.Context) error {
+	timeout := svc.config.GracefulShutdownTimeout
 	log.Event(ctx, "commencing graceful shutdown", log.Data{"graceful_shutdown_timeout": timeout}, log.INFO)
 	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
 
-	// stop any incoming requests before closing any outbound connections
-	if err := svc.server.Shutdown(ctx); err != nil {
-		log.Event(ctx, "failed to shutdown http server", log.Error(err), log.ERROR)
+	// track shutown gracefully closes up
+	var gracefulShutdown bool
+
+	go func() {
+		defer cancel()
+		var hasShutdownError bool
+
+		// stop healthcheck, as it depends on everything else
+		if svc.serviceList.HealthCheck {
+			svc.healthCheck.Stop()
+		}
+
+		// stop any incoming requests before closing any outbound connections
+		if err := svc.server.Shutdown(ctx); err != nil {
+			log.Event(ctx, "failed to shutdown http server", log.Error(err), log.ERROR)
+			hasShutdownError = true
+		}
+
+		// close API
+		if err := svc.api.Close(ctx); err != nil {
+			log.Event(ctx, "error closing API", log.Error(err), log.ERROR)
+			hasShutdownError = true
+		}
+
+		if !hasShutdownError {
+			gracefulShutdown = true
+		}
+	}()
+
+	// wait for shutdown success (via cancel) or failure (timeout)
+	<-ctx.Done()
+
+	if !gracefulShutdown {
+		err := errors.New("failed to shutdown gracefully")
+		log.Event(ctx, "failed to shutdown gracefully ", log.ERROR, log.Error(err))
+		return err
 	}
 
-	if err := svc.API.Close(ctx); err != nil {
-		log.Event(ctx, "error closing API", log.Error(err), log.ERROR)
-	}
-
-	log.Event(ctx, "graceful shutdown complete", log.INFO)
+	log.Event(ctx, "graceful shutdown was successful", log.INFO)
+	return nil
 }
 
-func registerCheckers(ctx context.Context, hc *healthcheck.HealthCheck) (err error) {
+func registerCheckers(ctx context.Context, hc HealthChecker) (err error) {
 	// TODO ADD HEALTH CHECKS HERE
 	return
 }
---
+++ service/service_test.go
@@ -0,0 +1,177 @@
+package service_test
+
+import (
+	"context"
+	"net/http"
+	"sync"
+	"testing"
+
+	"github.com/ONSdigital/dp-healthcheck/healthcheck"
+	"github.com/ONSdigital/dp-image-importer/config"
+	"github.com/ONSdigital/dp-image-importer/service"
+	"github.com/ONSdigital/dp-image-importer/service/mock"
+	serviceMock "github.com/ONSdigital/dp-image-importer/service/mock"
+	"github.com/pkg/errors"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	ctx           = context.Background()
+	testBuildTime = "BuildTime"
+	testGitCommit = "GitCommit"
+	testVersion   = "Version"
+)
+
+var (
+	errHealthcheck = errors.New("healthCheck error")
+)
+
+var funcDoGetHealthcheckErr = func(cfg *config.Config, buildTime string, gitCommit string, version string) (service.HealthChecker, error) {
+	return nil, errHealthcheck
+}
+
+var funcDoGetHTTPServerNil = func(bindAddr string, router http.Handler) service.HTTPServer {
+	return nil
+}
+
+func TestRun(t *testing.T) {
+
+	Convey("Having a set of mocked dependencies", t, func() {
+
+		hcMock := &serviceMock.HealthCheckerMock{
+			AddCheckFunc: func(name string, checker healthcheck.Checker) error { return nil },
+			StartFunc:    func(ctx context.Context) {},
+		}
+
+		serverWg := &sync.WaitGroup{}
+		serverMock := &serviceMock.HTTPServerMock{
+			ListenAndServeFunc: func() error {
+				serverWg.Done()
+				return nil
+			},
+		}
+
+		funcDoGetHealthcheckOk := func(cfg *config.Config, buildTime string, gitCommit string, version string) (service.HealthChecker, error) {
+			return hcMock, nil
+		}
+
+		funcDoGetHTTPServer := func(bindAddr string, router http.Handler) service.HTTPServer {
+			return serverMock
+		}
+
+		Convey("Given that initialising healthcheck returns an error", func() {
+			initMock := &serviceMock.InitialiserMock{
+				DoGetHTTPServerFunc:  funcDoGetHTTPServerNil,
+				DoGetHealthCheckFunc: funcDoGetHealthcheckErr,
+			}
+			svcErrors := make(chan error, 1)
+			svcList := service.NewServiceList(initMock)
+			_, err := service.Run(ctx, svcList, testBuildTime, testGitCommit, testVersion, svcErrors)
+
+			Convey("Then service Run fails with the same error and the flag is not set", func() {
+				So(err, ShouldResemble, errHealthcheck)
+				So(svcList.HealthCheck, ShouldBeFalse)
+			})
+		})
+
+		Convey("Given that all dependencies are successfully initialised", func() {
+
+			initMock := &serviceMock.InitialiserMock{
+				DoGetHTTPServerFunc:  funcDoGetHTTPServer,
+				DoGetHealthCheckFunc: funcDoGetHealthcheckOk,
+			}
+			svcErrors := make(chan error, 1)
+			svcList := service.NewServiceList(initMock)
+			serverWg.Add(1)
+			_, err := service.Run(ctx, svcList, testBuildTime, testGitCommit, testVersion, svcErrors)
+
+			Convey("Then service Run succeeds and all the flags are set", func() {
+				So(err, ShouldBeNil)
+				So(svcList.HealthCheck, ShouldBeTrue)
+			})
+
+			Convey("The checkers are registered and the healthcheck and http server started", func() {
+				So(len(hcMock.AddCheckCalls()), ShouldEqual, 0)
+				So(len(initMock.DoGetHTTPServerCalls()), ShouldEqual, 1)
+				So(initMock.DoGetHTTPServerCalls()[0].BindAddr, ShouldEqual, ":24800")
+				So(len(hcMock.StartCalls()), ShouldEqual, 1)
+				serverWg.Wait() // Wait for HTTP server go-routine to finish
+				So(len(serverMock.ListenAndServeCalls()), ShouldEqual, 1)
+			})
+		})
+
+	})
+}
+
+func TestClose(t *testing.T) {
+
+	Convey("Having a correctly initialised service", t, func() {
+
+		hcStopped := false
+
+		// healthcheck Stop does not depend on any other service being closed/stopped
+		hcMock := &serviceMock.HealthCheckerMock{
+			AddCheckFunc: func(name string, checker healthcheck.Checker) error { return nil },
+			StartFunc:    func(ctx context.Context) {},
+			StopFunc:     func() { hcStopped = true },
+		}
+
+		// server Shutdown will fail if healthcheck is not stopped
+		serverMock := &mock.HTTPServerMock{
+			ListenAndServeFunc: func() error { return nil },
+			ShutdownFunc: func(ctx context.Context) error {
+				if !hcStopped {
+					return errors.New("Server stopped before healthcheck")
+				}
+				return nil
+			},
+		}
+
+		Convey("Closing the service results in all the dependencies being closed in the expected order", func() {
+
+			initMock := &mock.InitialiserMock{
+				DoGetHTTPServerFunc: func(bindAddr string, router http.Handler) service.HTTPServer { return serverMock },
+				DoGetHealthCheckFunc: func(cfg *config.Config, buildTime string, gitCommit string, version string) (service.HealthChecker, error) {
+					return hcMock, nil
+				},
+			}
+
+			svcErrors := make(chan error, 1)
+			svcList := service.NewServiceList(initMock)
+			svc, err := service.Run(ctx, svcList, testBuildTime, testGitCommit, testVersion, svcErrors)
+			So(err, ShouldBeNil)
+
+			err = svc.Close(context.Background())
+			So(err, ShouldBeNil)
+			So(len(hcMock.StopCalls()), ShouldEqual, 1)
+			So(len(serverMock.ShutdownCalls()), ShouldEqual, 1)
+		})
+
+		Convey("If services fail to stop, the Close operation tries to close all dependencies and returns an error", func() {
+
+			failingserverMock := &mock.HTTPServerMock{
+				ListenAndServeFunc: func() error { return nil },
+				ShutdownFunc: func(ctx context.Context) error {
+					return errors.New("Failed to stop http server")
+				},
+			}
+
+			initMock := &mock.InitialiserMock{
+				DoGetHTTPServerFunc: func(bindAddr string, router http.Handler) service.HTTPServer { return failingserverMock },
+				DoGetHealthCheckFunc: func(cfg *config.Config, buildTime string, gitCommit string, version string) (service.HealthChecker, error) {
+					return hcMock, nil
+				},
+			}
+
+			svcErrors := make(chan error, 1)
+			svcList := service.NewServiceList(initMock)
+			svc, err := service.Run(ctx, svcList, testBuildTime, testGitCommit, testVersion, svcErrors)
+			So(err, ShouldBeNil)
+
+			err = svc.Close(context.Background())
+			So(err, ShouldNotBeNil)
+			So(len(hcMock.StopCalls()), ShouldEqual, 1)
+			So(len(failingserverMock.ShutdownCalls()), ShouldEqual, 1)
+		})
+	})
+}
